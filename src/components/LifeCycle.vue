2f1de19d461d464e9bc1c4dd074f6c8bb52e2b00
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-01-22 19:40:33 -0600

    tutorial concepts vuejs cli

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Meu nome eh: {{ nome }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      nome: "Nu stiu",
+    };
+  },
+  created() { // usado para criar o loader
+    //ao carregar pagina troca data
+    setTimeout(() => {
+      this.nome = "Luciius";
+    }, 1000);
+  },
+  mounted() {
+    // troca valor
+    setTimeout(() => {
+      this.nome = "Petris";
+    }, 2000);
+  },
+};
+</script>
---
+++ src/components/NovoComponente.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Ola ----</h1>
+    <p>O nome eh {{ nome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NovoComponente",
+  data() {
+    return {
+      nome: "Tuva",
+    };
+  },
+};
+</script>
