a15255422612e4b20f5f80b5173907d6fb297919
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-01-23 13:12:29 -0600

    update project

--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,7 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+
+</style>
--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -8,6 +8,9 @@
     <ul>
       <li :key="code" v-for="code in codes">{{code}}</li>
     </ul>
+
+    <button @click="showEmail()">{{textoBotao}}</button>
+
     <p v-show="show_email">Mande um email para: {{email}}</p>
     <!-- uso de v-bind -->
     <p>Para ir ao meu portfolio <a v-bind:href="linko" target="_blank">Aqui</a> </p>
@@ -23,10 +26,22 @@ export default {
   data(){
       return{
           laboras: true,
-          show_email: true,
+          show_email: false,
           codes: ['JS', 'PHP', 'Python', 'Java', 'Typescript'],
           email: 'email@ex.com',
-          linko: 'https://google.com'
+          linko: 'https://google.com',
+          textoBotao: 'Show Email'
+      }
+  },
+  methods: {
+      showEmail(){
+          this.show_email = !this.show_email
+          if(this.show_email){
+              this.textoBotao = "Ocultar Email"
+          }else{
+              this.textoBotao = "Mostrar Email"
+          }
+          
       }
   }
 };
