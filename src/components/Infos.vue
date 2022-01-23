5da49ddeeaac132778dd76b85b85a030053961f3
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-01-22 21:38:48 -0600

    update vue

--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <p>Estou laborando</p>
+      <!-- condicionais -->
+    <p v-if="laboras">Estou laborando</p>
+    <p v-else>Em busca de laboro</p>
+
     <p>Uso tecnoloogias:</p>
     <ul>
-      <li>JS</li>
-      <li>PHP</li>
-      <li>CSS</li>
+      <li :key="code" v-for="code in codes">{{code}}</li>
     </ul>
+    <p v-show="show_email">Mande um email para: {{email}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Infos",
+  data(){
+      return{
+          laboras: true,
+          show_email: true,
+          codes: ['JS', 'PHP', 'Python', 'Java', 'Typescript'],
+          email: 'email@ex.com'
+      }
+  }
 };
 </script>
