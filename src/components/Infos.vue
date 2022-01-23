e95bd6f9c72feb27bb990524a17cbe6ab913a9ed
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-01-22 21:58:05 -0600

    update project vbind

--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -9,18 +9,24 @@
       <li :key="code" v-for="code in codes">{{code}}</li>
     </ul>
     <p v-show="show_email">Mande um email para: {{email}}</p>
+    <!-- uso de v-bind -->
+    <p>Para ir ao meu portfolio <a v-bind:href="linko" target="_blank">Aqui</a> </p>
+    <pictures/>
   </div>
 </template>
 
 <script>
+import Pictures from './Pictures.vue';
 export default {
+  components: { Pictures },
   name: "Infos",
   data(){
       return{
           laboras: true,
           show_email: true,
           codes: ['JS', 'PHP', 'Python', 'Java', 'Typescript'],
-          email: 'email@ex.com'
+          email: 'email@ex.com',
+          linko: 'https://google.com'
       }
   }
 };
---
+++ src/components/Pictures.vue
@@ -0,0 +1,18 @@
+<template>
+  <!--alternativo v bind-->
+  <div>
+    <img :src="avatar" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Picture",
+  data() {
+    return {
+      avatar: "/img/pixs.png",
+      desc: "Fulano de tal",
+    };
+  },
+};
+</script>
